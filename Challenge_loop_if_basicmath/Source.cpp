110ae005c9a9d978e654eff4fd0ec4d08e12aed2
ธนกฤต ทองหล่อ <thanakrit_thonglow@hotmail.com>
2020-08-10 15:13:29 +0700

    Add project files.

---
+++ Challenge_loop_if_basicmath/Source.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+int a, b, t;
+float s, sx, xb;
+float xbar(int x, int y)
+{
+	for (int i = x; i <= y; i++)
+	{
+		s += i;
+	}
+	xb = s / (float)(y + 1 - x);
+	return s / (float)(y + 1 - x);
+}
+float SD(int x, int y)
+{
+	for (int i = x; i <= y; i++)
+	{
+		sx += pow(i - xb, 2);
+	}
+	sx /= (float)(y + 1 - x);
+	return sqrt(sx);
+}
+int main()
+{
+	scanf_s("%d %d", &a, &b);
+	if (a < b) for (int i = a; i <= b; i++) printf("%d ", i);
+	else for (int i = a; i >= b; i--) printf("%d ", i);
+	printf("\n");
+	if (a > b) t = a, a = b, b = t;
+	printf("Average = %.1f\n", xbar(a, b));
+	printf("SD = %.2f\n", SD(a, b));
+	return 0;
+}
